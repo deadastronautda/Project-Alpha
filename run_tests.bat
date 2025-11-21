50ffe88293820d09a24da09b8ab23362222c0b86
deadastronautda <230589515+deadastronautda@users.noreply.github.com>
2025-11-21 22:56:34 +0300

    $commitMessage

--- run_tests.bat
+++ run_tests.bat
@@ -2,7 +2,7 @@
 echo 🧪 Запуск тестов...
 call venv\Scripts\activate.bat
 pip install -r requirements.txt
-if exist requirements_test.txt pip install -r requirements_test.txt
+if exist requirements-test.txt pip install -r requirements-test.txt
 pytest --verbose tests/
 echo 📊 Тесты завершены
 pause
